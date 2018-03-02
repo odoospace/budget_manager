1b0bc41e76478979897ae63865eccc3923ea26f6
Pedro Gracia <pedro.gracia@impulzia.com>
2018-03-02 22:58:04 +0200

    * fixed a missing [] typo

--- models.py
+++ models.py
@@ -16,7 +16,7 @@ class crossovered_budget(models.Model):
             segment_tmpl_ids = []
             segment_ids = self.env.user.segment_ids
             for s in segment_ids:
-                segment_tmpl_ids += s.segment_id.segment_tmpl_id.id
+                segment_tmpl_ids += [s.segment_id.segment_tmpl_id.id]
                 segment_tmpl_ids += s.segment_id.segment_tmpl_id.get_childs_ids()
             virtual_segments = self.env['analytic_segment.template'].search([('virtual', '=', True)])
             segment_tmpl_ids += [i.id for i in virtual_segments]
@@ -30,7 +30,7 @@ class crossovered_budget(models.Model):
         segment_tmpl_ids = []
         segment_ids = user.segment_ids
         for s in segment_ids:
-            segment_tmpl_ids += s.segment_id.segment_tmpl_id.id
+            segment_tmpl_ids += [s.segment_id.segment_tmpl_id.id]
             segment_tmpl_ids += s.segment_id.segment_tmpl_id.get_childs_ids()
         virtual_segments = self.env['analytic_segment.template'].search([('virtual', '=', True)])
         segment_tmpl_ids += [i.id for i in virtual_segments]
@@ -50,7 +50,7 @@ class crossovered_budget(models.Model):
             segment_tmpl_ids = []
             segment_ids = self.env.user.segment_ids
             for s in segment_ids:
-                segment_tmpl_ids += s.segment_id.segment_tmpl_id.id
+                segment_tmpl_ids += [s.segment_id.segment_tmpl_id.id]
                 segment_tmpl_ids += s.segment_id.segment_tmpl_id.get_childs_ids()
             # add virtual companies segments
             virtual_segments = self.env['analytic_segment.template'].search([('virtual', '=', True)])
