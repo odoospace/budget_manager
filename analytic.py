c0ebc38128dca5b3cc8821369c6a3496d88e17ea
Pedro Gracia <pedro.gracia@impulzia.com>
2018-03-09 11:46:55 +0200

    + add group to analytic accounts

---
+++ analytic.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from openerp import models, fields, api
+
+class account_analytic_account(models.Model):
+    _inherit = 'account.analytic.account'
+
+    group = fields.Selection([
+        ('A', 'Gastos Secretarias'),
+        ('B', 'Gastos Areas y Equipos'),
+        ('C', 'Gastos Generales'),
+        ('D', 'Ingresos')
+    ])
