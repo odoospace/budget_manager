546dba8c14359a21d0f5424c3126f1bc4afdd9a5
Pedro Gracia <pedro.gracia@impulzia.com>
2019-01-08 08:39:26 +0100

    - add excel to grouped budgets

--- wizard_group.py
+++ wizard_group.py
@@ -57,14 +57,14 @@ class XLSXWizard(models.TransientModel):
             ('Unidades Funcionales y CCM', ['CCM']),
             ('Alquiler y Gastos de Oficina', ['CCE', 'CCA', 'CCM']),
             ('Asignaciones Autonómicas y Municipales', ['CCE']),
-            ('Asignaciones Autonómicas y Círculos', ['CCA']),
-            ('Asignaciones Círculos', ['CCA']),
+            ('Asignaciones Municipales y Círculos', ['CCA']),
+            ('Asignaciones Círculos', ['CCM']),
             ('Otros-', ['CCE', 'CCA', 'CCM']),
             # Ingresos
             ('Aportaciones GP', ['CCE', 'CCA', 'CCM']),
             ('Aportaciones Cargos P\xc3\xbablicos', ['CCE', 'CCA', 'CCM']),
             ('Colaboraciones Adscritas', ['CCE', 'CCA', 'CCM']),
-            ('Subvenciones', ['CCE', 'CCA', 'CCM']), # TODO: review CCA and CCM!!!!
+            ('Subvenciones', ['CCE']), # TODO: review CCA and CCM!!!!
             ('Estatal', ['CCA', 'CCM']),
             ('Otros+', ['CCE', 'CCA']),
             ('CCA', ['CCM'])
@@ -102,7 +102,7 @@ class XLSXWizard(models.TransientModel):
                     'ALQUILER Y GASTOS OFICINAS': 'Alquiler y Gastos de Oficina',
                     'COMISIONES BANCARIAS': 'Otros-',
                     'ASIGNACIONES AUTONÓMICAS': u'Asignaciones Autonómicas y Municipales',
-                    'ASIGNACIONES MUNICIPALES': u'Asignaciones Autonómicas y Círculos',
+                    'ASIGNACIONES MUNICIPALES': u'Asignaciones Municipales y Círculos',
                     'PROVISION CONTINGENCIAS': 'Otros-'
                 },
                 'Gastos Extraordinarios': {
@@ -119,9 +119,26 @@ class XLSXWizard(models.TransientModel):
         
         total_planned_amount = {}
         total_practical_amount = {}
+        budgets = {
+            'CCE': [],
+            'CCM': [],
+            'CCA': []
+        }
 
         for i in self.group_id.budget_ids:
-            category = i.category
+            # compatibility with names too if category isn't defined
+            if i.category:
+                category = i.category
+            elif 'CCE' in i.name:
+                category = 'CCE'
+            elif 'CCA' in i.name:
+                category = 'CCA'
+            elif 'CCM' in i.name:
+                category = 'CCM'
+
+            print 'category...', category
+
+            budgets[category].append(i)
             vals = {
                 'budget_id': i.id, 
                 'date_from': date_from,
@@ -129,9 +146,10 @@ class XLSXWizard(models.TransientModel):
             }
             budget_wiz = self.env['budget_manager.xlsxwizard'].create(vals)
             # get data from detail budget report
+            print '___'
             X, XX, groups, analytic_lines, analytic_lines_obj = budget_wiz.process_data(date_from, date_to)
-            results[i.id] = (X, XX, groups, analytic_lines, analytic_lines_obj)
-            print i.name, groups.keys()
+            results[i.id] = (X, XX, groups, analytic_lines, analytic_lines_obj) # TODO: review to use this
+            print '===', i.name, groups.keys()
 
             # reset totals (planed and practical)
             total_planned_amount[i] = {}
@@ -142,26 +160,37 @@ class XLSXWizard(models.TransientModel):
                     total_planned_amount[i][column] = 0
                     total_practical_amount[i][column] = 0
 
-            for k1, l1 in groups.items(): # level 1                  
+            print 'starting...'
+
+            for k1, l1 in groups.items(): # level 1
+                #print 'k1', k1    
                 for k2, l2 in l1.items(): # level 2
+                    #print 'k2', k2
                     for k3, l3 in l2.items(): # level 3
-                        #print 'k1:', k1, 'k2:', k2, 'k3:', k3
+                        #print 'k3', k3
+                        print 'k1:', k1, 'k2:', k2, 'k3:', k3
                         # check mapping for level 3
-                        for m in MAPPING[3]: 
+                        for m in MAPPING[3]:
+                            print m, MAPPING[3]
                             if k3 == m:
                                 column = MAPPING[3][m]
                                 # some name of columns are dynamic
                                 if '%s' in column:
                                     column = column % category
                                 # check type
+                                
                                 for ttype, v in l3:
                                     #print i, column, '...'
                                     if ttype == 1:
+                                        print 'ttype:', 1
                                         total_planned_amount[i][column] += v.planned_amount
                                         total_practical_amount[i][column] += v.practical_amount
                                     else:
+                                        print 'ttype:', ttype
                                         total_practical_amount[i][column] += v.amount
+                            stop
                         # check mapping for level 2
+                        stop
                         for m in MAPPING[2]:
                             for n in MAPPING[2][m]:
                                 if (n == '*' and k1 == m.decode('utf-8')) or (k1 == m.decode('utf-8') and k2 == n.decode('utf-8')):
@@ -188,20 +217,27 @@ class XLSXWizard(models.TransientModel):
 
         #print 'P:', total_planned_amount
         #print 'R:', total_practical_amount
-        stop
+        
+        #stop
+        
         # Create an new Excel file and add a worksheet
         # https://www.odoo.com/es_ES/forum/ayuda-1/question/return-an-excel-file-to-the-web-client-63980
         xlsxfile = StringIO.StringIO()
         workbook = xlsxwriter.Workbook(xlsxfile, {'in_memory': True})
         worksheet = workbook.add_worksheet()
-        worksheet.freeze_panes(1, 1) # freeze first column and first row
+        #xworksheet.freeze_panes(1, 1) # freeze first column and first row
 
         # styles
         _money = workbook.add_format({'num_format': '#,##0.00'})
         _porcentage = workbook.add_format({'num_format': '#,##0.00"%"', 'bg_color': '#92ff96'})
         _bold = workbook.add_format({'bold': True})
         _bold_center = workbook.add_format({'bold': True, 'align': 'center'})
-        _yellow = workbook.add_format({'bold': True, 'bg_color': 'yellow'})
+
+        _yellow = workbook.add_format({'bg_color': '#fbe5a3', 'num_format': '#,##0.00'})
+        _green = workbook.add_format({'bg_color': '#cbdeb9', 'num_format': '#,##0.00'})
+        _red = workbook.add_format({'bg_color': '#f0cdb1', 'num_format': '#,##0.00'})
+
+        """
         _silver_money = workbook.add_format({'bg_color': '#D0D0D0', 'num_format': '#,##0.00'})
         _silver_bold_center = workbook.add_format({'bold': True, 'bg_color': '#D0D0D0', 'align': 'center'})
         _gray = workbook.add_format({'bold': True, 'bg_color': 'silver'})
@@ -215,6 +251,35 @@ class XLSXWizard(models.TransientModel):
         _blue = workbook.add_format({'bold': True, 'bg_color': 'blue', 'font_color': 'white'})
         _blue_money = workbook.add_format({'bold': True, 'bg_color': 'blue', 'font_color': 'white', 'num_format': '#,##0.00'})
         _blue_porcentage = workbook.add_format({'bold': True, 'bg_color': 'blue', 'font_color': 'white', 'num_format': '#,##0.00"%"'})
+        """
+
+        # print columns
+        x = 0
+        for category in ['CCE', 'CCA', 'CCM']:
+            y = 0
+            for c in COLUMNS:
+                if category in c[1]:
+                    column = c[0].decode('utf-8')
+                    worksheet.set_column(x, x+1, 12)
+                    worksheet.merge_range(y, x, y, x+1, column, _gray)
+                    x += 2
+            y += 1
+        
+        workbook.close()
+
+        xlsxfile.seek(0)
+        name = self.budget_id.name.lower().replace(' ', '_')
+        vals = {
+            'name': 'presupuesto_agrupado_%s_%s_%s.xlsx' % (name, _date_from, _date_to),
+            'datas': base64.encodestring(xlsxfile.read()),
+            'datas_fname': 'presupuesto_agrupado_%s_%s_%s.xlsx' % (name, _date_from, _date_to),
+            'res_model': self.group_id._name,
+            'res_id': self.group_id.id,
+            'type': 'binary'
+        }
+        attachment_id = self.env['ir.attachment'].create(vals)
+
+        return True
 
 
         # header
