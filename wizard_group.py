ed7bc0b6e20390449cf849439d60e26c5db675f6
Pedro Gracia <pedro.gracia@impulzia.com>
2019-01-29 18:11:48 +0100

    - first version of grouped xlsx

--- wizard_group.py
+++ wizard_group.py
@@ -26,16 +26,24 @@ G = {
 
 class XLSXWizard(models.TransientModel):
     _name = 'budget_manager.group.xlsxwizard'
+    
+    @api.model
+    def _default_group(self):
+        res = self._context.get('active_id', False)
+        return res
 
-    group_id = fields.Many2one('crossovered.budget.group', string="Group", required=True)
+    group_id = fields.Many2one('crossovered.budget.group', default=_default_group, string="Group", required=True)
     date_from = fields.Date(required=True)
     date_to = fields.Date(required=True)
     incoming_bypass = fields.Boolean(default=False)
 
-
     @api.one
     def run_export_xlsx(self):
-        results = {}
+        res = {
+            'CCE': {},
+            'CCA': {},
+            'CCM': {}
+        }
         _date_to = self.date_to
         _date_from = self.date_from
         date_from = datetime.strptime(self.date_from, '%Y-%m-%d').date()
@@ -64,7 +72,7 @@ class XLSXWizard(models.TransientModel):
             ('Aportaciones GP', ['CCE', 'CCA', 'CCM']),
             ('Aportaciones Cargos P\xc3\xbablicos', ['CCE', 'CCA', 'CCM']),
             ('Colaboraciones Adscritas', ['CCE', 'CCA', 'CCM']),
-            ('Subvenciones', ['CCE', 'CCA']), # TODO: review CCA and CCM!!!!
+            ('Subvenciones', ['CCE']), # TODO: review CCA and CCM!!!!
             ('Estatal', ['CCA', 'CCM']),
             ('Otros+', ['CCE', 'CCA']),
             ('CCA', ['CCM'])
@@ -148,7 +156,7 @@ class XLSXWizard(models.TransientModel):
             # get data from detail budget report
             print '___'
             X, XX, groups, analytic_lines, analytic_lines_obj = budget_wiz.process_data(date_from, date_to)
-            results[i.id] = (X, XX, groups, analytic_lines, analytic_lines_obj) # TODO: review to use this
+            #res[i.id] = (X, XX, groups, analytic_lines, analytic_lines_obj) # TODO: review to use this
             print '===', i.name, groups.keys()
 
             # reset totals (planed and practical)
@@ -188,9 +196,9 @@ class XLSXWizard(models.TransientModel):
                                     else:
                                         print 'ttype:', ttype
                                         total_practical_amount[i][column] += v.amount
-                            # stop
+                            #stop
                         # check mapping for level 2
-                        # stop
+                        #stop
                         for m in MAPPING[2]:
                             for n in MAPPING[2][m]:
                                 if (n == '*' and k1 == m.decode('utf-8')) or (k1 == m.decode('utf-8') and k2 == n.decode('utf-8')):
@@ -201,24 +209,28 @@ class XLSXWizard(models.TransientModel):
                                     # check type
                                     for ttype, v in l3:
                                         if ttype == 1:
-                                            total_planned_amount[i][column] += v.planned_amount
-                                            total_practical_amount[i][column] += v.practical_amount
+                                            try:
+                                                total_planned_amount[i][column] += v.planned_amount
+                                                total_practical_amount[i][column] += v.practical_amount
+                                            except Exception as e:
+                                                print '***', e
+                                                if column == 'Subvenciones':
+                                                    _column = 'Otros+'
+                                                    total_planned_amount[i][_column] += v.planned_amount
+                                                    total_practical_amount[i][_column] += v.practical_amount
                                         else:
                                             total_practical_amount[i][column] += v.amount
             # print columns
+            res[category][i.name] = {}
             for c in COLUMNS:
                 if category in c[1]:
                     column = c[0].decode('utf-8')
-                    print c[0], total_planned_amount[i][column]
-                    print c[0], total_practical_amount[i][column]
-            #print 'P:', total_planned_amount
-            #print 'R:', total_practical_amount
-            #stop
-
-        #print 'P:', total_planned_amount
-        #print 'R:', total_practical_amount
-        
-        #stop
+                    res[category][i.name][c[0]] = {
+                        'planned': total_planned_amount[i][column],
+                        'practical': total_practical_amount[i][column]
+                    } 
+                    
+        #print '>>>', res
         
         # Create an new Excel file and add a worksheet
         # https://www.odoo.com/es_ES/forum/ayuda-1/question/return-an-excel-file-to-the-web-client-63980
@@ -236,7 +248,8 @@ class XLSXWizard(models.TransientModel):
         _yellow = workbook.add_format({'bg_color': '#fbe5a3', 'num_format': '#,##0.00'})
         _green = workbook.add_format({'bg_color': '#cbdeb9', 'num_format': '#,##0.00'})
         _red = workbook.add_format({'bg_color': '#f0cdb1', 'num_format': '#,##0.00'})
-        _gray = workbook.add_format({'bg_color': 'silver', 'num_format': '#,##0.00', 'bold': True})
+        _gray = workbook.add_format({'bold': True, 'bg_color': 'silver'})
+
         """
         _silver_money = workbook.add_format({'bg_color': '#D0D0D0', 'num_format': '#,##0.00'})
         _silver_bold_center = workbook.add_format({'bold': True, 'bg_color': '#D0D0D0', 'align': 'center'})
@@ -253,212 +266,38 @@ class XLSXWizard(models.TransientModel):
         _blue_porcentage = workbook.add_format({'bold': True, 'bg_color': 'blue', 'font_color': 'white', 'num_format': '#,##0.00"%"'})
         """
 
-        # print columns
-        x = 0
+        y = 0
         for category in ['CCE', 'CCA', 'CCM']:
-            y = 0
-            for c in COLUMNS:
-                if category in c[1]:
-                    column = c[0].decode('utf-8')
-                    worksheet.set_column(x, x+1, 12)
-                    worksheet.merge_range(y, x, y, x+1, column, _gray)
-                    x += 2
-            y += 1
+            # headers
+            if res[category]:
+                x = 1
+                worksheet.set_column(y, 0, 20)
+                worksheet.write(y, 0, category, _gray)
+                for c in COLUMNS:
+                    if category in c[1]:
+                        column = c[0].decode('utf-8')
+                        worksheet.set_column(x, x+1, 12)
+                        worksheet.merge_range(y, x, y, x+1, column, _gray)
+                        x += 2
+                for line in res[category]:
+                    y += 1
+                    x = 0
+                    worksheet.write(y, x, line, _gray)
+                    x += 1
+                    for c in COLUMNS:
+                        if category in c[1]:
+                            worksheet.write(y, x, res[category][line][c[0]]['planned'])
+                            worksheet.write(y, x+1, res[category][line][c[0]]['practical'])
+                            x += 2
+                y += 2
         
         workbook.close()
-
-        xlsxfile.seek(0)
-        name = self.group_id.name.lower().replace(' ', '_')
-        vals = {
-            'name': 'presupuesto_agrupado_%s_%s_%s.xlsx' % (name, _date_from, _date_to),
-            'datas': base64.encodestring(xlsxfile.read()),
-            'datas_fname': 'presupuesto_agrupado_%s_%s_%s.xlsx' % (name, _date_from, _date_to),
-            'res_model': self.group_id._name,
-            'res_id': self.group_id.id,
-            'type': 'binary'
-        }
-        attachment_id = self.env['ir.attachment'].create(vals)
-
-        return True
-
-
-        # header
-        y = 0
-        x = 1
-        worksheet.set_column(0, 0, 40)
-        date_from = datetime.strptime(_date_from, '%Y-%m-%d').strftime('%d/%m/%y')
-        date_to = datetime.strptime(_date_to, '%Y-%m-%d').strftime('%d/%m/%y')
-        name = '%s (%s - %s)' % (group_id.name, date_from, date_to)
-        worksheet.write(y, 0, name, _yellow)
-        for column in XX:
-            worksheet.set_column(x, x+1, 12)
-            worksheet.merge_range(y, x, y, x+1, column, _purple)
-            x += 2
-        x_total = x
-        worksheet.set_column(x, x+1, 12)
-        worksheet.merge_range(y, x, y, x+1, 'TOTAL', _red)
-        worksheet.write(y, x+2, 'DESV.', _red)
-
-        # process data
-        y += 1
-        #for row in groups:
-        _groups = [
-            'Gastos Secretarias', 'Gastos Areas y Equipos',
-            'Gastos Generales', 'Gastos Extraordinarios', 'Otros', 'No asignado']
-        for row in _groups:
-            if not groups.has_key(row):
-                continue
-            worksheet.write(y, 0, row.upper(), _bold)
-            x = 1
-            for column in XX:
-                worksheet.write(y, x, 'PRESUP.', _bold_center)
-                worksheet.write(y, x+1, 'REALES', _bold_center)
-                x += 2
-            worksheet.write(y, x, 'PRESUP.', _bold_center)
-            worksheet.write(y, x+1, 'REALES', _bold_center)
-            worksheet.write(y, x+2, '%', _bold_center)
-            y += 1
-            y0 = y
-            # budget data
-            _lines = collections.OrderedDict(sorted(groups[row].items()))
-            for line in _lines:
-                worksheet.write(y, 0, line.upper())
-                # TODO: remove columns ?
-                #columns = groups[row][line]
-                # do sums
-                for i, column in enumerate(XX):
-                    planned_amount = 0
-                    practical_amount = 0
-                    x = i * 2 + 1
-                    if groups[row][line].has_key(column):
-                        for ttype, l in groups[row][line][column]:
-                            if ttype == 1:
-                                # from budget
-                                planned_amount += l.planned_amount
-                                practical_amount += l.practical_amount
-                            elif ttype == 2:
-                                # from analytic
-                                practical_amount += l.amount
-                    # TODO: add euros
-                    worksheet.write(y, x, planned_amount, _money)
-                    worksheet.write(y, x+1, practical_amount, _silver_money)
-                # add X totals (red)
-                cell_range_planned = ''
-                cell_range_practical = ''
-                for i, column in enumerate(XX):
-                    x0 = i * 2 + 1
-                    cell_range_planned += '%s+' % xl_rowcol_to_cell(y, x0)
-                    cell_range_practical += '%s+' % xl_rowcol_to_cell(y, x0+1)
-                worksheet.write_formula(y, x+2, '{=%s}' % cell_range_planned[:-1], _money)
-                worksheet.write_formula(y, x+3, '{=%s}' % cell_range_practical[:-1], _silver_money)
-                # add %
-                cell_planned = xl_rowcol_to_cell(y, x+2)
-                cell_practical = xl_rowcol_to_cell(y, x+3)
-                worksheet.write_formula(y, x+4, '{=(%s/%s-1)*100}' % (cell_practical, cell_planned), _porcentage)
-                y += 1
-            # add Y total
-            worksheet.write(y, 0, 'TOTAL %s' % row.upper(), _gray)
-            for i in range(len(XX)+1): # add X total column too
-                x = i * 2 + 1
-                cell_range = xl_range(y0, x, y-1, x)
-                worksheet.write_formula(y, x, '{=SUM(%s)}' % cell_range, _gray_money)
-                cell_range = xl_range(y0, x+1, y-1, x+1)
-                worksheet.write_formula(y, x+1, '{=SUM(%s)}' % cell_range, _gray_money)
-                # add %
-                cell_planned = xl_rowcol_to_cell(y, x)
-                cell_practical = xl_rowcol_to_cell(y, x+1)
-                worksheet.write_formula(y, x+2, '{=(%s/%s-1)*100}' % (cell_practical, cell_planned), _gray_porcentage)
-            y += 1
-
-        # total
-        y += 1 # empty line
-        worksheet.write(y, 0, 'TOTAL GENERAL', _blue)
-        for i in range(len(XX)+1):
-            x = i * 2 + 1
-            cell_range = xl_range(1, x, y-1, x)
-            worksheet.write_formula(y, x, '{=SUM(%s)/2}' % cell_range, _blue_money)
-            cell_range = xl_range(1, x+1, y-1, x+1)
-            worksheet.write_formula(y, x+1, '{=SUM(%s)/2}' % cell_range, _blue_money)
-            # add %
-            cell_planned = xl_rowcol_to_cell(y, x)
-            cell_practical = xl_rowcol_to_cell(y, x+1)
-            worksheet.write_formula(y, x+2, '{=(%s/%s-1)*100}' % (cell_practical, cell_planned), _blue_porcentage)
-
-        y_total = y
-        y += 2
-
-        # special INCOMING part
-        # TODO: refactorize this!
-        row = 'Ingresos'
         
-        if not groups.has_key(row) and (self.incoming_bypass or self.group_id.zero_incoming):
-            groups[row] = {}
-        
-        worksheet.merge_range(y, x_total-3, y, x_total-1, row.upper(), _yellow )
-        #worksheet.set_column(x_total-1, x_total-1, 40)
-        #worksheet.write(y, x_total-1, row.upper(), _yellow)
-        worksheet.merge_range(y, x_total, y, x_total+1, 'TOTAL', _red)
-        worksheet.write(y, x_total+2, 'DESV.', _red)
-        y += 1
-        y_income = y
-        # budget data
-        _lines = collections.OrderedDict(sorted(groups[row].items()))
-        for line in _lines:
-            worksheet.merge_range(y, x_total-3, y, x_total-1, line.upper())
-            # TODO: remove columns ?
-            # columns = groups[row][line]
-            # do sums
-            column = 'INGRESOS'
-            planned_amount = 0
-            practical_amount = 0
-            if groups[row][line].has_key(column):
-                for ttype, l in groups[row][line][column]:
-                    if ttype == 1:
-                        # from budget
-                        planned_amount += l.planned_amount
-                        practical_amount += l.practical_amount
-                    elif ttype == 2:
-                        # from analytic
-                        practical_amount += l.amount
-            # TODO: add euros
-            worksheet.write(y, x_total, planned_amount, _money)
-            worksheet.write(y, x_total+1, practical_amount, _silver_money)
-            # add %
-            cell_planned = xl_rowcol_to_cell(y, x_total)
-            cell_practical = xl_rowcol_to_cell(y, x_total+1)
-            worksheet.write_formula(y, x+2, '{=(%s/%s-1)*100}' % (cell_practical, cell_planned), _porcentage)
-            y += 1
-
-        # total 'Ingresos'
-        y += 1 # empty line
-        worksheet.merge_range(y, x_total-3, y, x_total-1, 'TOTAL %s' % row.upper(), _blue)
-        cell_range = xl_range(y_income, x_total, y-1, x_total)
-        worksheet.write_formula(y, x_total, '{=SUM(%s)}' % cell_range, _blue_money)
-        cell_range = xl_range(y_income, x_total+1, y-1, x_total+1)
-        worksheet.write_formula(y, x_total+1, '{=SUM(%s)}' % cell_range, _blue_money)
-        # add %
-        cell_planned = xl_rowcol_to_cell(y, x_total)
-        cell_practical = xl_rowcol_to_cell(y, x_total+1)
-        worksheet.write_formula(y, x_total+2, '{=(%s/%s-1)*100}' % (cell_practical, cell_planned), _blue_porcentage)
-
-        # supertotal!
-        y += 2 # empty line
-        worksheet.merge_range(y, x_total-3, y, x_total-1, 'REMANENTE', _red_total)
-        cell_range_planned = '%s+%s' % (xl_rowcol_to_cell(y_total, x_total), xl_rowcol_to_cell(y-2, x_total))
-        cell_range_practical = '%s+%s' % (xl_rowcol_to_cell(y_total, x_total+1), xl_rowcol_to_cell(y-2, x_total+1))
-        worksheet.write_formula(y, x_total, '{=%s}' % cell_range_planned, _red_money)
-        worksheet.write_formula(y, x_total+1, '{=%s}' % cell_range_practical, _red_money)
-        # add %
-        cell_planned = xl_rowcol_to_cell(y, x_total)
-        cell_practical = xl_rowcol_to_cell(y, x_total+1)
-        worksheet.write_formula(y, x_total+2, '{=(%s/%s-1)*100}' % (cell_practical, cell_planned), _red_porcentage)
-
-
         # Rewind the buffer.
         xlsxfile.seek(0)
         name = self.group_id.name.lower().replace(' ', '_')
         vals = {
-            'name': 'presupuesto_%s_%s_%s.xlsx' % (name, _date_from, _date_to),
+            'name': 'presupuesto_agrupado_%s_%s_%s.xlsx' % (name, _date_from, _date_to),
             'datas': base64.encodestring(xlsxfile.read()),
             'datas_fname': 'presupuesto_%s_%s_%s.xlsx' % (name, _date_from, _date_to),
             'res_model': self.group_id._name,
